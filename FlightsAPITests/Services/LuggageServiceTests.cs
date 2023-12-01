02f24758f57a93291673014b1e7e2d323b8330af
Todor <84274078+TLizhev@users.noreply.github.com>
2023-12-01 13:22:26 +0000

    Add a test

---
+++ FlightsAPITests/Services/LuggageServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture;
+using FlightsAPI.Data.Models;
+using FlightsAPI.Repositories;
+using FlightsAPI.Services;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FlightsAPITests.Services
+{
+    public class LuggageServiceTests
+    {
+        private readonly LuggageService _sut;
+        private readonly Mock<ILuggageRepository> _luggageRepository;
+
+        public LuggageServiceTests()
+        {
+            _luggageRepository = new();
+            _sut = new(_luggageRepository.Object);
+        }
+
+        [Fact]
+        public void GetAllReturnsResult()
+        {
+            Fixture luggageFixture = new Fixture();
+            var list = luggageFixture.CreateMany<Luggage>(3).ToList();
+            _luggageRepository.Setup(x => x.GetAll()).Returns(list);
+
+            var result = _sut.GetLuggages();
+
+            result.Should().BeEquivalentTo(list);
+        }
+    }
+}
